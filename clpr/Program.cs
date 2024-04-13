0aead172806f0096cebd9ca77ade62dac0c335b6
TAT3MILLER <47550949+TAT3MILLER@users.noreply.github.com>
2024-04-12 21:58:34 -0400

    first commit

---
+++ clpr/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace clpr;
+
+internal class program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            var version = Assembly.GetEntryAssembly()?
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion
+                .ToString();
+            
+            Console.WriteLine($"clpr v{version}");
+            Console.WriteLine("----------------");
+            Console.WriteLine("\nUsage");
+            Console.WriteLine("  clpr <URL>");
+            
+        }
+
+        RunCmd(string.Join(' ', args));
+    }
+
+    static void RunCmd(string cmd)
+    {
+        
+    }
+
+    static bool ChangeDlDir()
+    {
+        
+
+        return true;
+    }
+}
+
